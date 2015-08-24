301d258756110897eb36323657a88ee2b03cd53a
unknown <307@M307-206.sit.nyp.edu.sg>
2015-08-24 11:50:15 +0800

    Game menu now with level select!

--- GuangZhe/main.cpp
+++ GuangZhe/main.cpp
@@ -8,30 +8,51 @@ void main()
 	title();
 	menu();
 	state choice = Play;
-	while (choice != MAX)
+	level number = All;
+	input(choice);
+	switch (choice)
 	{
-		input(choice);
-		switch (choice)
-		{
 		case Play:
 			cls();
-			level1();
-			level2();
+			title();
+			levelmenu();
+			levelselect(number);
+			switch (number)
+			{
+				case All:
+					cls();
+					level1();
+					level2();
+					break;
+				case lvl1:
+					cls();
+					level1();
+					break;
+				case lvl2:
+					cls();
+					level2();
+					break;
+				case clvl:
+					cls();
+					cout << "Errr waiting for custom map" << endl;
+					break;
+				default:
+					lvlerror();
+					choice = MAX;
+					break;
+			}
 			break;
 		case Create:
 			cls();
-			cout <<"ERROR WAITING FOR SHAWN TO COMPLETE HIS THINGY"<< endl;;
+			cout <<"ERROR WAITING FOR SHAWN TO COMPLETE HIS THINGY"<< endl;
 			break;
 		case Quit:
 			choice = MAX;
+			quit();
+			break;
 		default:
-			gotoXY(10,13);
-			cout << "Oops! Seems like you did not follow the instruction.";
-			gotoXY(10,14);
-			cout << "The game will now close!";
-			gotoXY(10,15);
+			error();
 			choice = MAX;
-		}
 	}
 }
 
@@ -65,8 +86,7 @@ void menu()
 void input(state& s)
 {
 	int num = 0;
-	gotoXY(10,12);
-	cout << "Please enter your choice (1,2 or 3 ONLY): ";
+	cout << "Please enter your choice (1, 2 or 3 ONLY): ";
 	cin >> num; 
 	s = static_cast<state>(num);
 }
@@ -77,3 +97,51 @@ void cls()
 	cout << string(2000,' ');
 	gotoXY(0,0);
 }
+
+void levelmenu()
+{
+	cout << "LEVEL SELECT";
+	gotoXY(10,9);
+	cout << "[0] All levels (1 and 2)";
+	gotoXY(10,10);
+	cout << "[1] Level 1";
+	gotoXY(10,11);
+	cout << "[2] Level 2";
+	gotoXY(10,12);
+	cout << "[3] Custom Level";
+	gotoXY(10,13);
+}
+
+void levelselect(level& l)
+{
+	int n = 0;
+	cout << "Please enter your choice (0, 1, 2 or 3 ONLY): ";
+	cin >> n;
+	l = static_cast<level>(n);
+}
+
+void error()
+{
+	gotoXY(10,13);
+	cout << "Oops! Seems like you did not follow the instruction.";
+	gotoXY(10,14);
+	cout << "The game will now close!";
+	gotoXY(10,15);
+}
+
+void lvlerror()
+{
+	gotoXY(10,14);
+	cout << "Oops! Seems like you did not follow the instruction.";
+	gotoXY(10,15);
+	cout << "The game will now close!";
+	gotoXY(10,16);
+}
+
+void quit()
+{
+	gotoXY(10,13);
+	cout <<"Thanks for playing the game!";
+	gotoXY(10,14);
+}
+
--- GuangZhe/main.h
+++ GuangZhe/main.h
@@ -6,12 +6,26 @@ enum state
 	Play = 1,
 	Create,
 	Quit,
-	MAX
+	MAX,
+};
+
+enum level
+{
+	All = 0,
+	lvl1,
+	lvl2,
+	clvl,
 };
 
 void title();
 void menu();
 void input(state& s);
 void cls();
+void levelmenu();
+void levelselect(level& l);
+void error();
+void lvlerror();
+void quit();
+void play();
 
 #endif
